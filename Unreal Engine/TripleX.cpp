93b457fc42d556daf423cc834eeb7de487946940
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-01-02 19:51:09 -0700

    Unreal: Variables and Terminal std::cout

--- Unreal Engine/TripleX.cpp
+++ Unreal Engine/TripleX.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+
 int main()
 {
+	std::cout << "You have a suitcase with a 3 digit combo lock" << std::endl;
+	std::cout << "Read the clues and input the prospective code" << std::endl;
+
+	int a = 4;
+	int b = 2;
+	int c = 7;
+
+	int sum = a + b + c;
+	int product = a * b * c;
+
+	std::cout << sum << std::endl;
+	stda::cout << product << std::endl;
+
 	return 0;
 }
