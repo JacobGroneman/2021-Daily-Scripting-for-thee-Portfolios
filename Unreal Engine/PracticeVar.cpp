f05ebf9f1672c1249e6bdce56eb83dadd69b749d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-01-03 22:41:04 -0700

    Unreal: Practiced Variables and Terminal std::cout

---
+++ Unreal Engine/PracticeVar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int value a = 5f;
+	float percentage = .56f;
+	
+		if (value > percentage)
+		{
+			std::cout << "Bork bork!" << std::endl;
+		}
+		else
+		{
+			std::cout << "No Borks!" << std::endl;
+		}
+}
