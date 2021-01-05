f5d6a6182e913a28ab31bd5ffd1fd6332d35a0b9
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-01-04 22:21:32 -0700

    Unreal: User Console Input (std::cin) and Comments
    TripleX.cpp

--- Unreal Engine/TripleX.cpp
+++ Unreal Engine/TripleX.cpp
@@ -1,19 +1,38 @@
+//Preprocessor Directives
 #include <iostream>
 
 int main()
 {
-	std::cout << "You have a suitcase with a 3 digit combo lock" << std::endl;
-	std::cout << "Read the clues and input the prospective code" << std::endl;
+	std::cout << "'You have a suitcase with a 3 digit combo lock. . .'" << std::endl;
+	std::cout << "Read the clues and input the prospective code." << std::endl;
 
-	const int a = 4; //Const prefix = unchanging var!
-	const int b = 2;
-	const int c = 7;
+	//Declaration Statements
+	const int DigitOne = 4; //Const prefix = unchanging var!
+	const int DigitTwo = 2;
+	const int DigitThree = 7;
+		int CodeSum = DigitOne + DigitTwo + DigitThree;
+		int CodeProduct = DigitOne * DigitTwo * DigitThree;
 
-	int sum = a + b + c;
-	int product = a * b * c;
+	//Expression Statements
+	std::cout << std::endl;
+	std::cout << "- There are 3 digits in the code." << std::endl;
+	std::cout << "- The digits add up to: " << CodeSum << std::endl;
+	std::cout << "- The digits multiply to give: " << CodeProduct << std::endl;
 
-	std::cout << sum << std::endl;
-	stda::cout << product << std::endl;
+	int GuessOne, GuessTwo, GuessThree;
+	int GuessSum = GuessOne + GuessTwo + GuessThree;
+	int GuessProduct = GuessOne * GuessTwo * GuessThree;
+		std::cin >> GuessOne
+		std::cin >> GuessTwo;
+		std::cin >> GuessThree;
+	
+			//std::cout << "You entered: " << GuessOne << GuessTwo << GuessThree;
 
-	return 0;
+	return 0; //Return Statement
 }
+
+//Single-line comment
+
+/*multi-
+line
+comment*/
