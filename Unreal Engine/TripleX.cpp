fd85fa59e2f42ac6b6a74490cdf884cd5755d643
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-01-06 17:52:55 -0700

    Unreal: Functions and "/n"

--- Unreal Engine/TripleX.cpp
+++ Unreal Engine/TripleX.cpp
@@ -1,41 +1,49 @@
 //Preprocessor Directives
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
-	std::cout << "'You have a suitcase with a 3 digit combo lock. . .'" << std::endl;
-	std::cout << "Read the clues and input the prospective code." << std::endl;
+	std::cout << "\"You have a suitcase with a 3 digit combo lock. . .\"\n";
+	std::cout << "Read the clues and input the prospective code.\n\n";
+}
+
+void PlayGame()
+{
+	PrintIntroduction()
 
 	//Declaration Statements
 	const int DigitOne = 4; //Const prefix = unchanging var!
 	const int DigitTwo = 2;
 	const int DigitThree = 7;
-		int CodeSum = DigitOne + DigitTwo + DigitThree;
-		int CodeProduct = DigitOne * DigitTwo * DigitThree;
+	int CodeSum = DigitOne + DigitTwo + DigitThree;
+	int CodeProduct = DigitOne * DigitTwo * DigitThree;
 
-	//Expression Statements
+	//Player Guess
 	std::cout << std::endl;
-	std::cout << "- There are 3 digits in the code." << std::endl;
-	std::cout << "- The digits add up to: " << CodeSum << std::endl;
-	std::cout << "- The digits multiply to give: " << CodeProduct << std::endl;
+	std::cout << "- There are 3 digits in the code.";
+	std::cout << "\n- The digits add up to: " << CodeSum;
+	std::cout << "\n- The digits multiply to give: " << CodeProduct << std::endl;
 
 	int GuessOne, GuessTwo, GuessThree;
 	int GuessSum = GuessOne + GuessTwo + GuessThree;
 	int GuessProduct = GuessOne * GuessTwo * GuessThree;
-		std::cin >> GuessOne
-		std::cin >> GuessTwo;
-		std::cin >> GuessThree;
-	
-			//std::cout << "You entered: " << GuessOne << GuessTwo << GuessThree;
+	std::cin >> GuessOne >> GuessTwo >> GuessThree;
 
-		if (GuessSum == CodeSum && GuessProduct == CodeProduct)
-		{
-			std::count << "The case clicks open. You Win." << std::endl;
-		}
-		else
-		{
-			std::count << "The case remains locked" << std::endl;
-		}
+
+	//Input Check
+	if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+	{
+		std::count << "\nThe case clicks open. You Win.";
+	}
+	else
+	{
+		std::count << "\nThe case remains locked.\n";
+	}
+}
+
+int main()
+{
+	PlayGame()
 
 	return 0; //Return Statement
 }
