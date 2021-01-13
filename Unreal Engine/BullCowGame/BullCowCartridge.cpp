68de626697b080cb6da7225aba2252a5377d3945
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-01-12 23:31:23 -0700

    Unreal: Member Variables (Header File Declarations)

--- Unreal Engine/BullCowGame/BullCowCartridge.cpp
+++ Unreal Engine/BullCowGame/BullCowCartridge.cpp
@@ -5,14 +5,23 @@ void UBullCowCartridge::BeginPlay() //Start
 {
 	Super::BeginPlay();
 	PrintLine(TEXT("Hi There! Welcome to the Bull Cow Game"));
+	PrintLine(TEXT("Guess the 4-letter word")); // Magic Number, Remove!
 	PrintLine(TEXT("Press Enter to continue. . ."));
+	
+	HiddenWord = TEXT("plant");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) //On Enter
 {
 	ClearScreen();
-
-	private FString HiddenWord = TEXT("plant");
 	
-	PrintLine(Input);
+	
+	if (Input == HiddenWord)
+	{
+		PrintLine(TEXT("Yee! You WON!"));
+	}
+	else
+	{
+		PrintLine(TEXT("oh no! You LOST!"));
+	}
 }
