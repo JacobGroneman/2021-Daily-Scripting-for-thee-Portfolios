d177ceed27a75b47b6615f543bca80681a08ac00
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-02-08 14:40:30 -0700

    Unity: Learn Ai - Primitive Crowd Simulation

---
+++ Unity Engine/Assets/Scripts/AiController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiController : MonoBehaviour
+{
+    public GameObject Goal;
+    private NavMeshAgent _agent;
+    
+    void Start()
+    {
+        _agent = this.GetComponent<NavMeshAgent>();
+        
+        _agent.SetDestination(Goal.transform.position);
+    }
+}
