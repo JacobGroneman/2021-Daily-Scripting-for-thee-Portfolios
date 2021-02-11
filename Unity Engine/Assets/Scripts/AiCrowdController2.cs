bb01e0b293dabd377a959282d0402d07afc8d303
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-02-11 00:28:43 -0700

    Unity: Learn Ai - Flee Locations

--- Unity Engine/Assets/Scripts/AiCrowdController2.cs
+++ Unity Engine/Assets/Scripts/AiCrowdController2.cs
@@ -11,6 +11,9 @@ public class AiCrowdController2 : MonoBehaviour
     private Animator _animator;
         private float _speedMultiplier;
 
+    private float _detectionRadius = 20f;
+    private float _fleeRadius = 10f;
+
     void Start()
     { 
         _goals = GameObject.FindGameObjectsWithTag("Goal");
@@ -22,6 +25,11 @@ public class AiCrowdController2 : MonoBehaviour
         _animator.SetFloat("walkingOffset", Random.Range(0 , 1));
         
         ResetAgent();
+    }
+
+    void Update()
+    {
+        OnGoalArrival();
     }
         
     private void ResetAgent()
@@ -35,5 +43,35 @@ public class AiCrowdController2 : MonoBehaviour
                 _animator.SetTrigger("isWalking");
             //Clears Agent Destination
                 _agent.ResetPath();
+        }
+    
+    private void OnGoalArrival()
+        {//resets Agent/Anim Stats and selects a new Agent Destination
+            if (_agent.remainingDistance < 1)
+            {
+                ResetAgent();
+                _agent.SetDestination(_goals[Random.Range(0, _goals.Length)].transform.position);
+            }        
+        }
+
+    public void DetectNewObstacle(Vector3 clickPos)
+        {
+            if (Vector3.Distance(clickPos, this.transform.position) < _detectionRadius)
+            {
+                Vector3 fleeDirection = (this.transform.position - clickPos).normalized; //Opposite of Direction
+                Vector3 newGoal = this.transform.position + (fleeDirection * _fleeRadius); //Place to run to
+                
+                NavMeshPath path = new NavMeshPath();
+                _agent.CalculatePath(newGoal, path); //Assigns the flee goal: "newGoal", to path.
+    
+                if (path.status != NavMeshPathStatus.PathInvalid) //If the path is Accessible.
+                {
+                    _agent.SetDestination(path.corners[path.corners.Length - 1]);
+                    
+                    _animator.SetTrigger("isRunning");
+                    _agent.speed = 10f;
+                    _agent.angularSpeed = 500f;
+                }
+            }
         }
 }
