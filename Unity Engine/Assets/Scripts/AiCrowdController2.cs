5ecffa606d6c76315a2206258e05327eb08d17d2
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-02-10 23:57:07 -0700

    Unity: Learn Ai - Flee

---
+++ Unity Engine/Assets/Scripts/AiCrowdController2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiCrowdController2 : MonoBehaviour
+{
+    private GameObject[] _goals;
+
+    private NavMeshAgent _agent;
+    private Animator _animator;
+        private float _speedMultiplier;
+
+    void Start()
+    { 
+        _goals = GameObject.FindGameObjectsWithTag("Goal");
+        
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.SetDestination(_goals[Random.Range(0, _goals.Length)].transform.position);
+        
+        _animator = GetComponent<Animator>(); 
+        _animator.SetFloat("walkingOffset", Random.Range(0 , 1));
+        
+        ResetAgent();
+    }
+        
+    private void ResetAgent()
+        {
+            //Value Assignments
+                _speedMultiplier = Random.Range(0.1f, 1.5f);
+                _agent.speed = _speedMultiplier * 2;
+                _agent.angularSpeed = 120f;
+                _animator.SetFloat("SpeedMultiplier", _speedMultiplier);
+            //Animation Assignment
+                _animator.SetTrigger("isWalking");
+            //Clears Agent Destination
+                _agent.ResetPath();
+        }
+}
---
+++ Unity Engine/Assets/Scripts/FleeObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleeObject : MonoBehaviour
+{
+    public GameObject Obstacle;
+    private GameObject[] _agents;
+    
+    void Start()
+    {
+        _agents = GameObject.FindGameObjectsWithTag("Agent");
+    }
+
+    void Update()
+    {
+        //Flee on Mouse Down
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hitInfo;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                
+                if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+                {
+                    Instantiate
+                        (Obstacle, hitInfo.point, Obstacle.transform.rotation);
+                    foreach (var agent in _agents)
+                    {
+                        agent.GetComponent<AiCrowdController2>().DetectNewObstacle(hitInfo.point);
+                    }
+                }
+            }
+    }
+}
