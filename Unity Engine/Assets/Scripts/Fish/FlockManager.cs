1729663231af6cfca55f25770312d4b0924a8683
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-02-12 14:41:08 -0700

    Unity: Learn Ai - Flocking Pt.1.1

---
+++ Unity Engine/Assets/Scripts/Fish/FlockManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject FishPrefab;
+    public int NumberOfFish = 20;
+    public GameObject[] Fishes;
+
+    [Header("Fish Settings")] //Sections off variables in the Inspector!
+    [Range(0.0f, 5.0f)] //Generates an Inspector Slider! (I want to research all these)
+    public float MinVelocity;
+    [Range(0.0f, 5.0f)] 
+    public float MaxVelocity;
+    
+    public Vector3 SwimLimits = new Vector3(5, 5, 5);
+
+    void Start()
+    {
+        Fishes = new GameObject[NumberOfFish];
+        
+        RandomizeFishStartPosition();
+    }
+
+    void Update()
+    {
+        
+    }
+        
+        private void RandomizeFishStartPosition()
+        { //Randomizes start Position on All Fishes
+          
+            for (int i = 0; i < NumberOfFish; i++)
+            {
+                Vector3 pos = this.transform.position + 
+                              new Vector3(Random.Range(-SwimLimits.x, SwimLimits.x),
+                                  Random.Range(-SwimLimits.y, SwimLimits.y),
+                                  Random.Range(-SwimLimits.z, SwimLimits.z));
+                Fishes[i] = Instantiate(FishPrefab, pos, Quaternion.identity);
+            }
+        }
+}
