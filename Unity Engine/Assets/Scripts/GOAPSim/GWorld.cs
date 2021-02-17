7071b1961cf5a7b31e0048299676a58bf9b2a993
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-02-16 18:20:21 -0700

    Unity: Learn Ai - Goal Action Oriented Programming - World States

---
+++ Unity Engine/Assets/Scripts/GOAPSim/GWorld.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GWorld //Sealed accesses the que's singularly. 
+{
+    private static readonly GWorld _instance = new GWorld(); //Only one Version on Game World Applicable!\
+    private static WorldStates _world;
+
+    static GWorld()
+    {
+        _world = new WorldStates();
+    }
+
+    private GWorld()
+    {
+        
+    }
+
+    public static GWorld Instance
+    {
+        get {return _instance;}
+    }
+
+    public WorldStates GetWorld()
+    {
+        return _world;
+    }
+}
---
+++ Unity Engine/Assets/Scripts/GOAPSim/WorldStates.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WorldState
+{
+    public string Key; //World State (ex: "# of Free Cubicles)
+    public int Value; //Index (ex. "5 Free Cubicles")
+}
+
+public class WorldStates
+{ 
+    public Dictionary<string, int> States;
+
+    public WorldStates()
+    {
+        States = new Dictionary<string, int>();
+    }
+    
+    public Dictionary<string, int> GetStates()
+    {
+        return States;
+    }
+
+    #region Conditions and Actions
+    
+        public bool HasState(string key)
+        {
+            return States.ContainsKey(key);
+        }
+        
+        private void AddState(string key, int value)
+        {
+            States.Add(key, value);
+        }
+    
+        private void RemoveState(string key)
+        {
+            if (States.ContainsKey(key))
+            {
+                States.Remove(key);
+            }
+        }
+    
+        public void ModifyState(string key, int value)
+        {
+            if (States.ContainsKey(key))
+            {
+                States[key] += value;
+    
+                if (States[key] <= 0)
+                {
+                    RemoveState(key);
+                }
+                else
+                {
+                    States.Add(key, value);
+                }
+            }
+        }
+        
+        public void SetState(string key, int value)
+        {
+            if (States.ContainsKey(key))
+            {
+                States[key] = value;
+            }
+            else
+            {
+                States.Add(key, value);
+            }
+        }
+        #endregion
+}
