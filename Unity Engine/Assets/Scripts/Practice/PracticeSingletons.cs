aecc614548fd5c18ef0a1961350bb5fba2e278b4
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-02-17 23:48:02 -0700

    Unity: Reviewed Singletons

---
+++ Unity Engine/Assets/Scripts/Practice/PracticeSingletons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//sorry about the rare crappy code below, it was a really busy day!!
+//But I got something in. Whoo!
+public class PracticeSingletons : MonoBehaviour
+{
+   private static PracticeSingletons _instance;
+
+   /*public static PracticeSingletons Instance;
+   {
+      get
+      {
+         if (_instance == null)
+         {
+            _instance = FindObjectOfType<PracticeSingletons>();
+            if (_instance == null)
+            {
+               _instance = new GameObject().AddComponent<PracticeSingletons>();
+            }
+            
+         }
+      }
+      return _instance;
+   }*/
+
+   private void Awake()
+   {
+      if (_instance != null)
+      {
+         Destroy(this);
+      }
+      
+      DontDestroyOnLoad(this); //Just for testing ;)
+   }
+}  
