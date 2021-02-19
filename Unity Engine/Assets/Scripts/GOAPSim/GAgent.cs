c15381049bcb4eba0bd2f4b7a31c102b777bc7a3
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-02-19 00:31:56 -0700

    Unity: Learn Ai - G.A.O.P. - Agents

--- Unity Engine/Assets/Scripts/GOAPSim/GAgent.cs
+++ Unity Engine/Assets/Scripts/GOAPSim/GAgent.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+public class SubGoal
+{
+    public Dictionary<string, int> SubGoals;
+    public bool Remove;
+
+    public SubGoal(string s, int i, bool r)
+    {
+        SubGoals = new Dictionary<string, int>();
+        SubGoals.Add(s, i);
+        Remove = r;
+    }
+}
 
 public class GAgent : MonoBehaviour
 {
+    public List<GAction> Actions = new List<GAction>();
+    public Dictionary<SubGoal, int> Goals = new Dictionary<SubGoal, int>();
+
+    //private GoalPlanner _planner;
+    private Queue<GAction> _actionQue;
+    public GAction CurrentAction;
+    private SubGoal _currentGoal;
+
     void Start()
     {
-        
+        GAction[] acts = this.GetComponents<GAction>();
+
+        foreach (GAction a in acts)
+        {
+            Actions.Add(a);
+        }
     }
     
-    void Update()
+    void LateUpdate()
     {
-        
+        //I need to work on the Planner before
     }
 }
