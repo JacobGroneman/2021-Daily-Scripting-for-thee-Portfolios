12ae766da5060f9367a00bae467b7bc27e93189b
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-02-18 23:03:57 -0700

    Unity: Learn Ai - G.A.O.P. - Actions

--- Unity Engine/Assets/Scripts/GOAPSim/GAction.cs
+++ Unity Engine/Assets/Scripts/GOAPSim/GAction.cs
@@ -1,16 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class GAction : MonoBehaviour
+public abstract class GAction : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public bool Running = false;
     
-    void Update()
+    public string ActionName = "Action";
+    public float Cost = 1.0f;
+    public float Duration;
+
+    public WorldState[] PreConditions, AfterEffects;
+    public Dictionary<string, int> Preconditions, Effects;
+    
+    public WorldState AgentBeliefs;
+    
+    public GameObject Target;
+    public GameObject TargetTag;
+    
+    public NavMeshAgent Agent;
+
+    public GAction() //Constructor
     {
-        
+        Preconditions = new Dictionary<string, int>();
+        Effects = new Dictionary<string, int>();
     }
+
+    public void Awake()
+        {
+            Agent = this.gameObject.GetComponent<NavMeshAgent>();
+    
+            if (PreConditions != null)
+            {
+                foreach (WorldState w in PreConditions)
+                {
+                    Preconditions.Add(w.Key, w.Value);
+                }
+            }
+            
+            if (AfterEffects != null)
+            {
+                foreach (WorldState w in AfterEffects)
+                {
+                    Effects.Add(w.Key, w.Value);
+                }
+            }
+        }
+    
+        public bool IsAchievable()
+        {
+            return true;
+        }
+
+        public bool IsAchievableGiven(Dictionary<string, int> conditions)
+        {
+            foreach (KeyValuePair<string, int> p in Preconditions)
+            {
+                if (!conditions.ContainsKey(p.Key))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public abstract bool PrePerform(); //For customizable conditions 
+        public abstract bool PostPerform();
 }
