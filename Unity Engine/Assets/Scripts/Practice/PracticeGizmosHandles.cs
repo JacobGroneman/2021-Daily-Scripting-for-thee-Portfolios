7ff66c14697eb8e05df2c54e84b79d4fd5d9076b
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-02-24 23:55:55 -0700

    Unity: Had a simple look at Unity Editor Gizmos

---
+++ Unity Engine/Assets/Scripts/Practice/PracticeGizmosHandles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Sorry about the short code, I taught class until 9:30pm
+
+public class PracticeGizmosHandles : MonoBehaviour
+{
+    public GameObject SampleManager;
+
+    public float ShieldArea; //For Handle
+    
+    void Update()
+    {
+        OnDrawGizmosSelected(SampleManager, 10f);
+    }
+
+        void OnDrawGizmosSelected(GameObject manager, float gizmoSize)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(manager.transform.position, gizmoSize /*(Radius)*/);
+        }
+}
