0f60ea332032d480f4ec302c786e6b192ae61644
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-03-01 18:42:45 -0700

    Unity: Learn Ai - Adding More Resources to the World Pt.1

--- Unity Engine/Assets/Scripts/GOAPSim/GWorld.cs
+++ Unity Engine/Assets/Scripts/GOAPSim/GWorld.cs
@@ -5,30 +5,47 @@ using UnityEngine;
 public sealed class GWorld //Sealed accesses the que's singularly. 
 {
     private static readonly GWorld _instance = new GWorld(); //Only one Version on Game World Applicable!\
+    
     private static WorldStates _world;
+    
     private static Queue<GameObject> _patients;
+    private static Queue<GameObject> _cubicles;
 
-    static GWorld()
-    {//Initializations
-        _world = new WorldStates();
-        _patients = new Queue<GameObject>();
-    }
-
-    private GWorld()
-    {}
-
-    public static GWorld Instance
-    {
-        get {return _instance;}
-    }
-
+    #region Constructors & Singletons
+        static GWorld()
+        {//Initializations
+            _world = new WorldStates();
+            _patients = new Queue<GameObject>();
+            //Cubicles
+            _cubicles = new Queue<GameObject>();
+            GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cubicle");
+                foreach (GameObject c in cubes) 
+                { 
+                    _cubicles.Enqueue(c); 
+                }
+                if (cubes.Length > 0)
+                {
+                    _world.ModifyState("FreeCubicle", cubes.Length);
+                }
+        }
+    
+        private GWorld()
+        {}
+    
+        public static GWorld Instance
+        {
+            get {return _instance;}
+        }
+        #endregion
+        
     public WorldStates GetWorld()
     {
         return _world;
     }
 
-    #region Character World Stats
+    #region World Queues
     
+    //Patient Queue
         public void AddPatient(GameObject patient)
             {
                 _patients.Enqueue(patient);
@@ -42,6 +59,20 @@ public sealed class GWorld //Sealed accesses the que's singularly.
             }
             return _patients.Dequeue();
         }
+        
+    //Cubicle Queue
+        public void AddCubicle(GameObject patient)
+            {
+                _cubicles.Enqueue(patient);
+            }
+                    
+            public GameObject RemoveCubicle()
+            {
+                if (_cubicles.Count == 0)
+                {
+                    return null;
+                }
+                return _cubicles.Dequeue();
+            }
             #endregion
-   
 }
