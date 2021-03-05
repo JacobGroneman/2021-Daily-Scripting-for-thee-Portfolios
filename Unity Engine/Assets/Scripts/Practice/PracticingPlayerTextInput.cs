f3292651107e6ebaacc3d0b261d1d7c577493ae1
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-03-05 14:43:23 -0700

    Practiced Player Text Input Quickly Before Work

---
+++ Unity Engine/Assets/Scripts/Practice/PracticingPlayerTextInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PracticingPlayerTextInput : MonoBehaviour
+{
+    public string PlayerName; //Input Field
+    public GameObject InputField;
+    public GameObject TextDisplay;
+
+    public void StoreName() //Stores player name and Displays it
+    {
+        PlayerName = InputField.GetComponent<Text>().text;
+        
+        TextDisplay.GetComponent<Text>().text =
+            "Welcome " + PlayerName + " to the game!";
+    }
+    
+    //Simple and Quick Before Work
+}
