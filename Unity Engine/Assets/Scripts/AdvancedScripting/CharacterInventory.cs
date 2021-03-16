462fe34d78324cb6441072221c96d25bedc2521a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-03-15 23:34:04 -0600

    Unity: Inventory Systems - Adding Function Stubs for Char. Inventory

---
+++ Unity Engine/Assets/Scripts/AdvancedScripting/CharacterInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInventory : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+
+        void StoreItem() {}
+    
+        void TryPickUp() {}
+    
+        void AddItemToInv() {}
+    
+        void AddItemToHotBar() {}
+
+        void DisplayInventory() {}
+        
+        void FillInventoryDisplay() {}
+        
+        void TriggerItemUse() {}
+}
