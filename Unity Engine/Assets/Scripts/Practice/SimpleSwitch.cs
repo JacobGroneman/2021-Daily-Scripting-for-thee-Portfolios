420f4470864c3f2673dfcf20cfabfe820a2ae27d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-03-23 23:12:48 -0600

    Revised Swith/Enum Applications

--- Unity Engine/Assets/Scripts/Practice/SimpleSwitch.cs
+++ Unity Engine/Assets/Scripts/Practice/SimpleSwitch.cs
@@ -5,54 +5,50 @@ using UnityEngine;
 public class SimpleSwitch : MonoBehaviour
 {
     private GameMode _gameMode;
-
-    private enum GameMode
-        {
-            Active,
-            Paused,
-            GameOver
-        }
-
+        private enum GameMode
+            {
+                Active,
+                Paused,
+                GameOver
+            }
+    private float _timeScale;
     private bool
         _isMobile,
         _isSpawning;
 
-    private float _timeScale;
-
     void Update()
     {
+        #region Game Constants
+            Time.timeScale = _timeScale;    
+            GameModeSettings();
+            #endregion
+            
         #region #Input
-        //Save/Load/ToggleMenus
+        //- - GameMode Toggles - -
+        //Pause Game
             if (Input.GetKeyDown(KeyCode.Escape))
-            {//Pause Game
+            {
                 if (_gameMode == GameMode.Active)
                 {
                     _gameMode = GameMode.Paused;
-                    ApplyGameModeSettings();
                 }
                 else if (_gameMode == GameMode.Paused)
                 {
                     _gameMode = GameMode.Active;
-                    ApplyGameModeSettings();
                 }
             }
-
+        //Game Over Manual
             if (Input.GetKey(KeyCode.D) &&
                 Input.GetKey(KeyCode.I) &&
                 Input.GetKey(KeyCode.E) && (_gameMode == GameMode.Active))
-            {//Game Over Manual
+            {
                 _gameMode = GameMode.GameOver;
-                ApplyGameModeSettings();
             }
             #endregion
-            
-            #region Master Properties
-                Time.timeScale = _timeScale;
-                #endregion
     }
 
     #region Save/Load/ToggleMenus
-        void ApplyGameModeSettings() 
+        void GameModeSettings() 
         {
             switch (_gameMode)
             {
