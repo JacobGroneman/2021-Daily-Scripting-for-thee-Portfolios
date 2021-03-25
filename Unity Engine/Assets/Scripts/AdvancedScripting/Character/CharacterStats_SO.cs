65338b84500f90d9f04d1f0c24de2c66b68dd688
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-03-24 23:02:14 -0600

    Unity: Inventory Systems - Equipping Weapons and Armor Pt.1.1

--- Unity Engine/Assets/Scripts/AdvancedScripting/Character/CharacterStats_SO.cs
+++ Unity Engine/Assets/Scripts/AdvancedScripting/Character/CharacterStats_SO.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats_SO : MonoBehaviour
-{//Need these scripts written!
-    public bool UnEquipWeapon() {};
-    public bool EquipWeapon() {};
+{ 
+    public bool UnEquipWeapon
+        (ItemPickup weaponPickup, CharacterInventory charInventory, GameObject weapon)
+    {
+        bool previousWeaponSame = false;
 
-    public bool UnEquipArmor() {};
-    public bool EquipArmor() {};
+        if (weapon != null)
+        {
+            if (weapon == weaponPickup)
+            {
+                previousWeaponSame = true;
+            }
+            
+            charInventory.inventoryDisplaySlots[2].sprite = null;
+            DestroyObject(weaponSlot.transform.GetChild(0).gameObject); //Interesting Method
+            weapon = null;
+            currentDamage = baseDamage;
+        }
+
+        return previousWeaponSame;
+    }
+
+    public void EquipWeapon
+        (ItemPickup weaponPickup, CharacterInventory charInventory, GameObject weapon)
+    {
+        Rigidbody newWeapon;
+
+        weapon = weaponPickup;
+        charInventory.inventoryDisplaySlots[2].sprite = weaponPickup.ItemDefinition.itemIcon;
+        newWeapon = Instantiate(weaponPickup.ItemDefinition.weaponSlotObject, weaponSlot.transform);
+        currentDamage = baseDamage + weapon.itemDefinition.itemAmount;
+    }
+
+    public bool UnEquipArmor() {}
+    public bool EquipArmor() {}
 }
