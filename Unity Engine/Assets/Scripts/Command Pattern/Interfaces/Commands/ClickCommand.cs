934d389839e144c834c839a87e6f4abd8d20e6aa
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-03-27 23:17:44 -0600

    Unity: Command Pattern - Implementing the Command Pattern

---
+++ Unity Engine/Assets/Scripts/Command Pattern/Interfaces/Commands/ClickCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickCommand : ICommand
+{
+    private readonly GameObject _cube;
+    private readonly Color _color; //(readonly prohibits changing values in ClickCommand)
+    private Color _previousColor;
+    
+    public ClickCommand(GameObject cube, Color color)
+    {
+        this._cube = cube;
+        this._color = color;
+    }
+    
+        public void Execute()
+        {
+            _previousColor = 
+                _cube.GetComponent<MeshRenderer>().material.color;
+            
+            _cube.GetComponent<MeshRenderer>().material.color =
+                _color;
+        }
+    
+        public void Undo()
+        {
+            _cube.GetComponent<MeshRenderer>().material.color = 
+                _previousColor;
+        }
+}
---
+++ Unity Engine/Assets/Scripts/Command Pattern/Interfaces/UserInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    void Update()
+    {
+        #region Mouse
+        //Left
+            if (Input.GetMouseButtonDown(0))
+            {
+                CursorChangeObjectColor();
+            }
+            #endregion
+    }
+    
+    #region Actions
+        void CursorChangeObjectColor()
+        {
+            Ray rayOrigin = 
+                Camera.main.ScreenPointToRay(Input.mousePosition);
+                
+            RaycastHit hitInfo;
+    
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                if (hitInfo.collider.tag == "cube")
+                {
+                    ICommand click = new ClickCommand(
+                            hitInfo.collider.gameObject,
+                            new Color(Random.value, Random.value, Random.value));
+                }
+            }
+        }
+        #endregion
+}
