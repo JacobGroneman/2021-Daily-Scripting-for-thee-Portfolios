a1fba6bd6f2290ecdef66bd15347f932fca98783
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-01 20:34:46 -0600

    Unity: Reviewed Singleton Design Pattern

---
+++ Unity Engine/Assets/Scripts/Practice/Singletons/SGameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SGameManager : MonoBehaviour
+{
+    private static SGameManager _instance;
+    
+    public static SGameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("SGameManager._instance = NULL!");
+            }
+            
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+        public void DisplayName()
+        {
+            Debug.Log("The Name is Pickles Baxter!");
+        }
+    
+}
---
+++ Unity Engine/Assets/Scripts/Practice/Singletons/sPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sPlayer : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SGameManager.Instance.DisplayName();
+        }
+    }
+}
