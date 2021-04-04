25edeef22c6521c11f281289915d2f11cdf447c5
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-03 22:36:08 -0600

    Reviewed the MonoSingleton

---
+++ Unity Engine/Assets/Scripts/Practice/Singletons/MonoSingletonReview.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoSingletonReview<T> : MonoBehaviour where T : MonoSingletonReview<T>
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.Log(typeof(T).ToString() + " is NULL!");
+            }
+
+            return _instance;
+        }
+    }
+    
+    private void Awake()
+    {
+        _instance = this as T;
+        Initialize();
+    }
+        public virtual void Initialize()
+        {}
+}
