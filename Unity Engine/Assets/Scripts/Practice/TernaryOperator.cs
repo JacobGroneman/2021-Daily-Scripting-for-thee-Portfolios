9c4be21b14a7f17b5c8b0c826898754c97bdd60c
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-05 21:50:40 -0600

    Unity: Ternary Operators

---
+++ Unity Engine/Assets/Scripts/Practice/TernaryOperator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Windows;
+
+public class TernaryOperator : MonoBehaviour
+{
+    private int _health = 10;
+    private bool _isGameOver;
+
+    void Update()
+    {
+        _isGameOver = _health > 10 ? false : true; //These are rad!
+        
+        /*Jet Brains recommends reducing the ternary operator as follows...:
+         _isGameOver = _health <= 10;*/
+
+        if (_isGameOver)
+        {
+            Debug.Log("You Lost the Game!");
+        }
+    }
+        private void OnColliderEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Spike")
+            {
+                _health--;
+            }
+        }
+}
