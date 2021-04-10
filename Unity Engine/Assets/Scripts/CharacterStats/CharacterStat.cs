3cec32f9365810b0ee6ae966ee1456d3024f6485
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-10 14:54:04 -0600

    Unity: Character Stats Pt.2

--- Unity Engine/Assets/Scripts/CharacterStats/CharacterStat.cs
+++ Unity Engine/Assets/Scripts/CharacterStats/CharacterStat.cs
@@ -1,15 +1,57 @@
-﻿using System.Collections.Generic; //Not MonoBehavior
+﻿using System; //For the Rounder
+using System.Collections.Generic;
+using UnityEngine;
+
+//Not MonoBehavior
 
 public class CharacterStat
 {
-    public float BaseValue;
-    public float Value {get {return CalculateFinalValue();}}
     private readonly List<StatModifier> _statModifiers;
-
+    public float BaseValue;
+       
+    public float Value //This is rad
+    {
+            get
+            {
+                if (_isDirty)
+                {
+                    _value = CalculateFinalValue();
+                    _isDirty = false;
+                }
+                return _value;
+            }
+    }
+        private float _value;
+        private bool _isDirty = true;
+        
     //Constructor
         public CharacterStat(float baseValue)
         {
             BaseValue = baseValue;
             _statModifiers = new List<StatModifier>();
         }
+
+        #region Modifying Value
+            public void AddModifier(StatModifier mod)
+            {
+                _isDirty = true;
+                _statModifiers.Add(mod);
+            }
+            public bool RemoveModifier(StatModifier mod)
+            {
+                _isDirty = true;
+                return _statModifiers.Remove(mod);
+            }
+            private float CalculateFinalValue()
+            {
+                float finalValue = BaseValue;
+
+                for (int i = 0; i < _statModifiers.Count; i++)
+                {
+                    finalValue += _statModifiers[i].Value;
+                }
+
+                return (float)Math.Round(finalValue, 4); //Rounder
+            }
+            #endregion
 }
