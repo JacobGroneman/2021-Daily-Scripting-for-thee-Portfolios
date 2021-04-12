75b082538777a1a8b93a8023f454a3e2b0971fe1
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-11 22:00:40 -0600

    Unity: Character Stats Pt.3

--- Unity Engine/Assets/Scripts/CharacterStats/CharacterStat.cs
+++ Unity Engine/Assets/Scripts/CharacterStats/CharacterStat.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class CharacterStat
 {
     private readonly List<StatModifier> _statModifiers;
+
     public float BaseValue;
        
     public float Value //This is rad
@@ -32,10 +33,18 @@ public class CharacterStat
         }
 
         #region Modifying Value
+        private int CompareModifierOrder(StatModifier a, StatModifier b)
+        {
+            if (a.Order < b.Order) {return - 1;}
+            else if (a.Order > b.Order) {return 1;}
+            //if(a.Order == b.Order)
+            return 0;
+        }
             public void AddModifier(StatModifier mod)
             {
                 _isDirty = true;
                 _statModifiers.Add(mod);
+                _statModifiers.Sort(CompareModifierOrder);
             }
             public bool RemoveModifier(StatModifier mod)
             {
@@ -48,10 +57,18 @@ public class CharacterStat
 
                 for (int i = 0; i < _statModifiers.Count; i++)
                 {
-                    finalValue += _statModifiers[i].Value;
+                    StatModifier mod = _statModifiers[i];
+                        if (mod.Type == StatModType.Flat)
+                        {
+                            finalValue += mod.Value;
+                        }
+                        else if (mod.Type == StatModType.Percent)
+                        {
+                            finalValue *= 1 + mod.Value;
+                        }
                 }
 
-                return (float)Math.Round(finalValue, 4); //Rounder
+                return (float)Math.Round(finalValue, 4); //Rounder (4 is sig.figs.)
             }
             #endregion
 }
--- Unity Engine/Assets/Scripts/CharacterStats/StatModifier.cs
+++ Unity Engine/Assets/Scripts/CharacterStats/StatModifier.cs
@@ -1,10 +1,22 @@
 ﻿public class StatModifier
 {
+  public readonly int Order;
   public readonly float Value;
+  public readonly StatModType Type;
 
   //Constructor
-    public StatModifier(float value)
+    public StatModifier(float value, StatModType type, int order)
     {
-      Value = value;
-    }
+      this.Value = value;
+      this.Type = type;
+      this.Order = order;
+    } 
+      public StatModifier // Automatic Constructor Setting for DefaultsS
+        (float value, StatModType type) : this(value, type, (int)type){}
 }
+
+public enum StatModType
+{
+  Flat,
+  Percent
+}//This is a cool concept
