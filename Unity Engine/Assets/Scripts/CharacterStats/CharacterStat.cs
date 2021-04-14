24b0b68d6568c35f653b88e42c426c3e563c220d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-13 22:14:05 -0600

    Unity: Character Stats Pt.5
    Changed Access Modifiers to Protected and Methods Virtual

--- Unity Engine/Assets/Scripts/CharacterStats/CharacterStat.cs
+++ Unity Engine/Assets/Scripts/CharacterStats/CharacterStat.cs
@@ -2,118 +2,139 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel; //For ReadOnlyCollection
 using UnityEngine;
-
-//Not MonoBehavior
-
-public class CharacterStat
+    
+namespace Jacob.CharacterStats{
+    
+[Serializable]
+public class CharacterStat //Not MonoBehavior
 {
     //ReadOnly let's players see the modifiers in-game while private
     private readonly ReadOnlyCollection<StatModifier> StatModifiers;
-    private readonly List<StatModifier> _statModifiers;
+    protected readonly List<StatModifier> _statModifiers;
 
     public float BaseValue;
-    private float lastBaseValue = float.MinValue;
-       
-    public float Value //This is rad
+    protected float lastBaseValue = float.MinValue;
+
+    public virtual float Value //This is rad
     {
-            get
-            {
-                if (_isDirty || lastBaseValue != BaseValue)
-                {
-                    lastBaseValue = BaseValue;
-                    _value = CalculateFinalValue();
-                    _isDirty = false;
-                }
-                return _value;
-            }
-    }
-        private float _value;
-        private bool _isDirty = true;
-        
-    //Constructor
-        public CharacterStat(float baseValue)
+        get
         {
-            BaseValue = baseValue;
-            _statModifiers = new List<StatModifier>();
-            StatModifiers = _statModifiers.AsReadOnly();//cool stuffs
+            if (_isDirty || lastBaseValue != BaseValue)
+            {
+                lastBaseValue = BaseValue;
+                _value = CalculateFinalValue();
+                _isDirty = false;
+            }
+
+            return _value;
+        }
+    }
+
+    protected float _value;
+    protected bool _isDirty = true;
+
+    //Constructors
+    public CharacterStat() //Parameterless for NullReferenceExceptions
+    {
+        _statModifiers = new List<StatModifier>();
+        StatModifiers = _statModifiers.AsReadOnly(); //cool stuffs
+    }
+
+    public CharacterStat(float baseValue) : this()
+    {
+        BaseValue = baseValue;
+    }
+
+    #region Modifying Value
+
+    //Sort Modifier Order
+    protected virtual int CompareModifierOrder(StatModifier a, StatModifier b)
+    {
+        if (a.Order < b.Order)
+        {
+            return -1;
+        }
+        else if (a.Order > b.Order)
+        {
+            return 1;
         }
 
-        #region Modifying Value
-    //Sort Modifier Order
-        private int CompareModifierOrder(StatModifier a, StatModifier b)
-        {
-            if (a.Order < b.Order) {return - 1;}
-            else if (a.Order > b.Order) {return 1;}
-            //if(a.Order == b.Order)
-            return 0;
-        }
+        //if(a.Order == b.Order)
+        return 0;
+    }
+
     //Add Modifiers
-            public void AddModifier(StatModifier mod)
+    public virtual void AddModifier(StatModifier mod)
+    {
+        _isDirty = true;
+        _statModifiers.Add(mod);
+        _statModifiers.Sort(CompareModifierOrder);
+    }
+
+    //Remove Modifier(s)
+    public virtual bool RemoveModifier(StatModifier mod)
+    {
+        if (_statModifiers.Remove(mod))
+        {
+            _isDirty = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    public virtual bool RemoveAllModifiersFromSource(object source)
+    {
+        bool didRemove = false;
+
+        for (int i = _statModifiers.Count - 1; i >= 0; i--)
+        {
+            //(I reverse i to prevent list shifting)
+            if (_statModifiers[i].Source == source)
             {
                 _isDirty = true;
-                _statModifiers.Add(mod);
-                _statModifiers.Sort(CompareModifierOrder);
+                didRemove = true;
+                _statModifiers.RemoveAt(i);
             }
-    //Remove Modifier(s)
-            public bool RemoveModifier(StatModifier mod)
-            {
-                if (_statModifiers.Remove(mod))
-                {
-                    _isDirty = true;
-                    return true;
-                }
-                
-                return false;
-            }
-            public bool RemoveAllModifiersFromSource(object source)
-            {
-                bool didRemove = false;
+        }
 
-                for (int i = _statModifiers.Count-1; i >= 0; i--)
-                {//(I reverse i to prevent list shifting)
-                    if (_statModifiers[i].Source == source)
-                    {
-                        _isDirty = true;
-                        didRemove = true;
-                        _statModifiers.RemoveAt(i);
-                    }
-                }
+        return didRemove;
+    }
 
-                return didRemove;
-            }
     //Calculate Modifiers to Value
-            private float CalculateFinalValue()
+    protected virtual float CalculateFinalValue()
+    {
+        float finalValue = BaseValue;
+        float sumPercentAdd = 0; // for StatModType.PercentAdd
+
+        for (int i = 0; i < _statModifiers.Count; i++)
+        {
+            StatModifier mod = _statModifiers[i];
+
+            if (mod.Type == StatModType.Flat)
             {
-                float finalValue = BaseValue;
-                float sumPercentAdd = 0; // for StatModType.PercentAdd
-
-                for (int i = 0; i < _statModifiers.Count; i++)
-                {
-                    StatModifier mod = _statModifiers[i];
-                    
-                        if (mod.Type == StatModType.Flat)
-                        {
-                            finalValue += mod.Value;
-                        }
-                        else if (mod.Type == StatModType.PercentAdd)
-                        {
-                            sumPercentAdd += mod.Value;
-                            
-                            //When all mods are accounted or mod != type
-                                if (i + 1 >= _statModifiers.Count || 
-                                    _statModifiers[i + 1].Type != StatModType.PercentAdd)
-                                {
-                                    finalValue *= 1 + sumPercentAdd; //times sum to final value
-                                    sumPercentAdd = 0; //reset the sum tally for reuse
-                                }
-                        }
-                        else if (mod.Type == StatModType.PercentMulti)
-                        {
-                            finalValue *= 1 + mod.Value;
-                        }
-                }
-
-                return (float)Math.Round(finalValue, 4); //Rounder (4 is sig.figs.)
+                finalValue += mod.Value;
             }
-            #endregion
-}
+            else if (mod.Type == StatModType.PercentAdd)
+            {
+                sumPercentAdd += mod.Value;
+
+                //When all mods are accounted or mod != type
+                if (i + 1 >= _statModifiers.Count ||
+                    _statModifiers[i + 1].Type != StatModType.PercentAdd)
+                {
+                    finalValue *= 1 + sumPercentAdd; //times sum to final value
+                    sumPercentAdd = 0; //reset the sum tally for reuse
+                }
+            }
+            else if (mod.Type == StatModType.PercentMulti)
+            {
+                finalValue *= 1 + mod.Value;
+            }
+        }
+
+        return (float) Math.Round(finalValue, 4); //Rounder (4 is sig.figs.)
+    }
+    #endregion
+} 
+    }
--- Unity Engine/Assets/Scripts/CharacterStats/StatModifier.cs
+++ Unity Engine/Assets/Scripts/CharacterStats/StatModifier.cs
@@ -1,32 +1,47 @@
-﻿public class StatModifier
+﻿
+namespace Jacob.CharacterStats
 {
-  public readonly int Order;
-  public readonly float Value;
-  public readonly StatModType Type;
-  public readonly object Source;
+  public class StatModifier
+  {
+    public readonly int Order;
+    public readonly float Value;
+    public readonly StatModType Type;
+    public readonly object Source;
 
-  #region Constructor/Default Settings
-  //Constructor
+    #region Constructor/Default Settings
+
+    //Constructor
     public StatModifier(float value, StatModType type, int order, object source)
     {
       this.Value = value;
       this.Type = type;
       this.Order = order;
       this.Source = source;
-    } 
-  //Default Settings  
-      public StatModifier//required:[value][type] / Set:[Order = (int)type][Source = null]
-        (float value, StatModType type) : this(value, type, (int)type, null){}
-      public StatModifier//required:[value][type][order] / Set:[source = null]
-        (float value, StatModType type, int order) : this(value, type, order, null){}
-      public StatModifier//required:[value][type][source] / Set:[Order = (int)type]
-          (float value, StatModType type, object source) : this(value, type, (int)type, source){}
-  #endregion
-}  
+    }
 
-public enum StatModType
-{
-  Flat = 100,
-  PercentAdd = 200,
-  PercentMulti = 300
-}//This enum utilization concept is cool
+    //Default Settings  
+    public StatModifier //required:[value][type] / Set:[Order = (int)type][Source = null]
+      (float value, StatModType type) : this(value, type, (int) type, null)
+    {
+    }
+
+    public StatModifier //required:[value][type][order] / Set:[source = null]
+      (float value, StatModType type, int order) : this(value, type, order, null)
+    {
+    }
+
+    public StatModifier //required:[value][type][source] / Set:[Order = (int)type]
+      (float value, StatModType type, object source) : this(value, type, (int) type, source)
+    {
+    }
+
+    #endregion
+  }
+
+  public enum StatModType
+  {
+    Flat = 100,
+    PercentAdd = 200,
+    PercentMulti = 300
+  } //This enum utilization concept is cool
+    }
