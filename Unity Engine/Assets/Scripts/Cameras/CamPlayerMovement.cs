0e95e5b84940a10bb803f2182a2782c78b51fb52
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-15 23:07:00 -0600

    Unity: Smooth 2D Camera Motion

---
+++ Unity Engine/Assets/Scripts/Cameras/CamPlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPlayerMovement : MonoBehaviour
+{
+    public float Velocity = 20;
+    private Vector2 _motion;
+    
+    void Update()
+    {
+        _motion = new Vector2
+            (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
+        transform.Translate(_motion * Velocity * Time.deltaTime);
+    }
+}
---
+++ Unity Engine/Assets/Scripts/Cameras/CamTracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTracker : MonoBehaviour
+{
+    public Transform TrackedObject;
+    
+    public float UpdateVelocity = 3;
+    
+    public Vector2 TrackingOffset;
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = (Vector3)TrackingOffset; //I'm researching what "(Vector3)" does.
+
+        _offset.z =
+            transform.position.z - TrackedObject.position.z;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.MoveTowards
+            (transform.position, TrackedObject.position + _offset, 
+            UpdateVelocity * Time.deltaTime); //I likee
+    }
+}
