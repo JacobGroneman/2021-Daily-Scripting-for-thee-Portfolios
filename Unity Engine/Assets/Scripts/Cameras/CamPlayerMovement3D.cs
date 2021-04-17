d0363322543ea3ef9310314615969b0067ad8d31
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-16 22:21:56 -0600

    Unity: Smooth 3D Camera Motion

---
+++ Unity Engine/Assets/Scripts/Cameras/CamPlayerMovement3D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPlayerMovement3D : MonoBehaviour
+{
+    public float Velocity = 20;
+    private Vector3 _motion;
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        #region Initialize
+            _rb = GetComponent<Rigidbody>();
+            #endregion
+    }
+    
+    void Update()
+    {
+        #region Movement
+            _motion = new Vector3
+                    (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            _rb.velocity = _motion * Velocity;
+            #endregion
+    }
+}
---
+++ Unity Engine/Assets/Scripts/Cameras/CamPlayerTracker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPlayerTracker : MonoBehaviour
+{
+    public Transform TrackedObject;
+
+    public float
+        MaxDistance = 10f,
+        MoveVelocity = 20f,
+        UpdateVelocity = 10f;
+    
+    [Range(0, 10)] public float CurrentDistance = 5;
+    private string _moveAxis = "Mouse ScrollWheel";
+    
+    private GameObject _ahead;
+    
+    private MeshRenderer _renderer;
+    public float HideDistance = 1.5f;
+
+    void Start()
+    {
+        #region Initialize
+            _ahead = new GameObject("Ahead");
+            _renderer = TrackedObject.gameObject.GetComponent<MeshRenderer>();
+            #endregion
+    }
+    
+    void Update()
+    {
+        #region Tracking
+            _ahead.transform.position =
+                TrackedObject.position + TrackedObject.forward
+                * (MaxDistance * 0.25f); // 1/4 of Max
+            
+            CurrentDistance += 
+                Input.GetAxisRaw(_moveAxis) * MoveVelocity * Time.deltaTime;
+            
+            CurrentDistance = Mathf.Clamp //Returns if in range
+                (CurrentDistance, 0, MaxDistance);
+            
+            transform.position = Vector3.MoveTowards
+                (transform.position,
+                TrackedObject.position + Vector3.up * CurrentDistance
+                - TrackedObject.forward * (CurrentDistance + MaxDistance * 0.5f), 
+                UpdateVelocity * Time.deltaTime); //Jees! (I like these .MoveTowards)
+            
+            transform.LookAt(_ahead.transform);//Stabilize
+            
+            _renderer.enabled = (CurrentDistance > HideDistance);//Toggles 1st & 3rd person
+            #endregion
+    }
+}
