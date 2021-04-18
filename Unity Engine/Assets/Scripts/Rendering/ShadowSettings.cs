46756f31415eb0b56c1a30db505c5121bc034e35
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-17 21:41:49 -0600

    Unity: Quickly Looked at the Scriptable Render Pipeline (SRP)
    This is rad stuffs

---
+++ Unity Engine/Assets/Scripts/Rendering/ShadowSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ShadowSettings : MonoBehaviour
+{
+    [Min(0f)] public float MaxDistance = 100f;
+
+    public enum TextureSize
+    {
+        _256 = 256, _512 = 512, 
+        _1024 = 1024, _2048 = 2048,
+        _4096 = 4096, _8192 = 8192
+    }
+    
+        [System.Serializable]
+        public struct Directional
+        {
+            public TextureSize AtlasSize;
+        }
+    
+        public Directional directional = new Directional()
+        {
+            AtlasSize = TextureSize._1024
+        };
+        
+        //I'm excited to learn this stuff
+}
+
+
