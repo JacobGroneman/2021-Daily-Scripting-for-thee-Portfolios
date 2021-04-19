0fe79464eb67c179b188b2520693e449553d47fc
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-18 23:25:30 -0600

    Unity: Procedural Grid Pt.2

--- Unity Engine/Assets/Scripts/Advanced Scripting/Mesh/Grids/Grid.cs
+++ Unity Engine/Assets/Scripts/Advanced Scripting/Mesh/Grids/Grid.cs
@@ -20,30 +20,48 @@ public class Grid : MonoBehaviour
         {
             WaitForSeconds wait = new WaitForSeconds(0.05f);
 
-            GetComponent<MeshFilter>().mesh = _mesh = new Mesh(); //Assigning Simultaneously
-            _mesh.name = "Procedural Grid";
-            
-            _vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-                _mesh.vertices = _vertices;
-                
-            int[] triangles = new int[3];
-            //These will form a square
-                triangles[0] = 0;
-                triangles[3] = triangles[2] = 1;
-                triangles[4] = triangles[1] = xSize + 1;
-                triangles[5] = xSize + 2; 
-            _mesh.triangles = triangles; //Forward faces have clockwise vertices
-                
-                for (int i = 0, y = 0; y <= ySize; y++)
-                {
-                    for (int x = 0; x <= xSize; x++, i++)
+            //Vertex & UV
+                _vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+                Vector2[] uv = new Vector2[_vertices.Length];
+                Vector4[] tangents = new Vector4[_vertices.Length];
+                    Vector4 tangent = new Vector4(1f, 0, 0, -1f);
+                    
+                    for (int i = 0, y = 0; y <= ySize; y++)
                     {
-                        _vertices[i] = new Vector3(x, y); //Freakin' radzies (I Love This)
-                        yield return wait;
+                        for (int x = 0; x <= xSize; x++, i++)
+                        {
+                            _vertices[i] = new Vector3(x, y); //Freakin' radzies (I Love This)
+                            uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                            tangents[i] = tangent;
+                            yield return wait;
+                        }
                     }
-                }
+                _mesh.vertices = _vertices;
+                _mesh.uv = uv;
+                _mesh.tangents = tangents;
+            
+            //Mesh
+                GetComponent<MeshFilter>().mesh = _mesh = new Mesh(); //Assigning Simultaneously
+                _mesh.name = "Procedural Grid";
+                    
+                int[] triangles = new int[xSize * ySize *  6]; //6 = vertices
+                
+                    for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) //This will form squares fully-mapped 
+                    {
+                        for (int x = 0; x < xSize; x++, ti += 6, vi++)
+                        {
+                            triangles[ti] = vi;
+                            triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                            triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                            triangles[ti + 5] = vi + xSize + 2;
+                            
+                                _mesh.triangles = triangles; //Forward faces have clockwise vertices
+                                _mesh.RecalculateNormals();
+                                yield return wait;
+                        }
+                    }
         }
-        
+    
         private void OnDrawGizmos()
         {
             if (_vertices == null) return; //For Edit Mode Errors
