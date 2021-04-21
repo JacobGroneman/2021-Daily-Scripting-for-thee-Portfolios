225230197a13da7145defe2d464a5f61f0a96854
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-04-20 22:51:51 -0600

    Unity: Persisting Objects Pt.2

--- Unity Engine/Assets/Scripts/Advanced Scripting/Object Management/Game.cs
+++ Unity Engine/Assets/Scripts/Advanced Scripting/Object Management/Game.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using File = UnityEngine.Windows.File;
 
 public class Game : MonoBehaviour
 {
@@ -9,13 +11,19 @@ public class Game : MonoBehaviour
 
     public KeyCode //This is nice
         CreateKey = KeyCode.C,
-        NewGameKey = KeyCode.N;
+        NewGameKey = KeyCode.N,
+        SaveKey = KeyCode.S,
+        LoadKey = KeyCode.L;
+
+    private string _savePath;
 
     void Awake()
     {
         #region Initialize
             _objects = new List<Transform>();
-            #endregion
+            _savePath = Path.Combine
+                (Application.persistentDataPath, "saveFile");
+                #endregion
     }
     
     void Update()
@@ -27,6 +35,14 @@ public class Game : MonoBehaviour
         else if(Input.GetKey(NewGameKey))
         {
             BeginNewGame();
+        }
+        else if(Input.GetKeyDown(SaveKey))
+        {
+            Save();
+        }
+        else if(Input.GetKeyDown(LoadKey))
+        {
+            Load();
         }
     }
 
@@ -51,6 +67,43 @@ public class Game : MonoBehaviour
             }
             _objects.Clear();
         }
+        private void Save()
+        {
+            using (var writer = new BinaryWriter //O.o Wow.
+                (System.IO.File.Open(_savePath, FileMode.Create)))
+            {
+                writer.Write(_objects.Count);
+
+                for (int i = 0; i < _objects.Count; i++)
+                {
+                    Transform t = _objects[i];
+                        writer.Write(t.localPosition.x);
+                        writer.Write(t.localPosition.y);
+                        writer.Write(t.localPosition.z);
+                }
+            }
+        }
+        private void Load()
+        {
+            BeginNewGame();
+            
+            using (var reader = new BinaryReader //O.o Wow.
+                (System.IO.File.Open(_savePath, FileMode.Open)))
+            {
+                int count = reader.ReadInt32();
+
+                for (int i = 0; i < count; i++)
+                {
+                    Vector3 p;
+                        p.x = reader.ReadSingle();
+                        p.y = reader.ReadSingle();
+                        p.z = reader.ReadSingle();
+
+                    Transform t = Instantiate(Prefab);
+                        t.localPosition = p;
+                        _objects.Add(t);
+                }
+            }
+        }
         #endregion
-    
 }
