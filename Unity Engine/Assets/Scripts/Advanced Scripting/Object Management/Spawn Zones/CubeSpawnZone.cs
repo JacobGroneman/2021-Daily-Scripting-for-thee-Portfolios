3c97c94829f8598c86456ef81eeb996716ac318a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-05-03 14:57:34 -0600

    Unity: Spawn Zones Pt.2

---
+++ Unity Engine/Assets/Scripts/Advanced Scripting/Object Management/Spawn Zones/CubeSpawnZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawnZone : SpawnZone
+{
+    private bool _surfaceOnly;
+    public override Vector3 SpawnPoint
+    {
+        get
+        {
+            Vector3 p;
+                p.x = Random.Range(-0.5f, 0.5f);
+                p.y = Random.Range(-0.5f, 0.5f);
+                p.z = Random.Range(-0.5f, 0.5f);
+                
+                if (_surfaceOnly)
+                { 
+                    int axis = Random.Range(0, 3);//(x, y, z)
+                    p[axis] = p[axis] < 0f ? -0.5f : 0.5f; //Parameter Snapping
+                }
+                
+            return transform.TransformPoint(p);
+        }
+    }
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+        }
+}
--- Unity Engine/Assets/Scripts/Advanced Scripting/Object Management/Spawn Zones/SpawnZone.cs
+++ Unity Engine/Assets/Scripts/Advanced Scripting/Object Management/Spawn Zones/SpawnZone.cs
@@ -5,13 +5,4 @@ using UnityEngine;
 public abstract class SpawnZone : MonoBehaviour
 { 
     public abstract Vector3 SpawnPoint {get;}
-
-    #region Debug
-            private void OnDrawGizmos()
-            {
-                Gizmos.color = Color.cyan;
-                Gizmos.matrix = transform.localToWorldMatrix;
-                Gizmos.DrawWireSphere(Vector3.zero, 1f);
-            }
-            #endregion
 }
