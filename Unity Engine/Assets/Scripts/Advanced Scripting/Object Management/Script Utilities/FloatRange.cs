e8f7eff596b54e4161e331bf386a0242bc3eff49
Jacob Groneman <jacobjazzdrummer@gmail.com>
2021-05-12 23:13:13 -0600

    Unity: Configuring Shapes Pt.4
    [3.6]

---
+++ Unity Engine/Assets/Scripts/Advanced Scripting/Object Management/Script Utilities/FloatRange.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct FloatRange
+{
+    public float Min, Max;
+
+    public float RandomValueInRange
+    {
+        get {return Random.Range(Min, Max);}
+    }
+}
--- Unity Engine/Assets/Scripts/Advanced Scripting/Object Management/Spawn Zones/SpawnZone.cs
+++ Unity Engine/Assets/Scripts/Advanced Scripting/Object Management/Spawn Zones/SpawnZone.cs
@@ -5,43 +5,53 @@ using UnityEngine;
 public abstract class SpawnZone : PersistableObject
 { 
     public abstract Vector3 SpawnPoint {get;}
-
-    public enum SpawnMovementDirection
-    {Forward, Upward, Outward, Random}
-        [SerializeField]
-        private SpawnMovementDirection _spawnMovementDirection;
-
+    
+    [System.Serializable]
+    public struct SpawnConfiguration
+    {
+        public enum MovementDirection
+        {Forward, Upward, Outward, Random}
+            public MovementDirection movementDirection;
+        
+        public FloatRange Speed;
+    }
         [SerializeField] 
-        private float _spawnSpeedMin, _spawnSpeedMax;
-
+        private SpawnConfiguration _spawnConfig;
+    
     #region Configuration
-       public virtual void ConfigureSpawn(vShape shape)
+        public virtual void ConfigureSpawn(vShape shape)
         {
             Transform t = shape.transform;
-            t.localPosition = SpawnPoint;
-            t.localRotation = Random.rotation;
-            t.localScale = Vector3.one * Random.Range(0.1f, 1f);
-                
+                t.localPosition = SpawnPoint;
+                t.localRotation = Random.rotation;
+                t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+    
             shape.SetColor(Random.ColorHSV
-            (hueMin:0, hueMax:1f, saturationMin: 0.5f, saturationMax: 1f,
-                valueMin: 0.25f, valueMax: 1f, alphaMin: 1f, alphaMax:1f));
-
-            shape.AngularVelocity = 
+                (hueMin: 0, hueMax: 1f, saturationMin: 0.5f, saturationMax: 1f,
+                    valueMin: 0.25f, valueMax: 1f, alphaMin: 1f, alphaMax: 1f));
+    
+            shape.AngularVelocity =
                 Random.onUnitSphere * Random.Range(0f, 50f); //ran range/1 sec
-            
+    
             Vector3 direction;
-                if (_spawnMovementDirection == SpawnMovementDirection.Upward)
-                {direction = transform.up;}
-                else if (_spawnMovementDirection == SpawnMovementDirection.Outward)
-                {direction = (t.localPosition - transform.position).normalized;}
-                else if (_spawnMovementDirection == SpawnMovementDirection.Random)
-                {direction = Random.onUnitSphere;}
-                else
-                {direction = transform.forward;}
-            
-                shape.Velocity =
-                    direction * Random.Range(_spawnSpeedMin, _spawnSpeedMax);
+                switch (_spawnConfig.movementDirection)
+                {
+                    case SpawnConfiguration.MovementDirection.Upward:
+                        direction = transform.up;
+                        break;
+                    case SpawnConfiguration.MovementDirection.Outward:
+                        direction = (t.localPosition - transform.position).normalized;
+                        break;
+                    case SpawnConfiguration.MovementDirection.Random:
+                        direction = Random.onUnitSphere;
+                        break;
+                    default:
+                        direction = transform.forward;
+                        break;
+                }
+
+                shape.Velocity = direction * _spawnConfig.Speed.RandomValueInRange;
         }
-       #endregion
+        #endregion
 }
     
